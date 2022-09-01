fea2a5031d16197e18e5d0d44865014838af2704
!am9a09e <gg9a09e@gmail.com>
2022-09-02 00:02:24 +0800

    506.相对名次

--- 506.相对名次.py
+++ 506.相对名次.py
@@ -11,5 +11,14 @@ class Solution(object):
         :type score: List[int]
         :rtype: List[str]
         """
+        desc = ("Gold Medal", "Silver Medal", "Bronze Medal")
+        ans = [""] * len(score)
+        arr = sorted(enumerate(score), key=lambda x: x[1], reverse=True)
+        # arr = sorted(enumerate(score), key=lambda x: -x[1])
+        for i, (idx, _) in enumerate(arr):
+            ans[idx] = desc[i] if i < 3 else str(i + 1)
+        return ans
+
 # @lc code=end
 
+print(Solution().findRelativeRanks([10,3,8,9,4]))
