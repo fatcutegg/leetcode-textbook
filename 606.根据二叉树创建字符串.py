7b2a7d50a8062b950af9239e6921ee4402a3b5b1
!am9a09e <gg9a09e@gmail.com>
2022-09-17 00:00:53 +0800

    606.根据二叉树创建字符串

---
+++ 606.根据二叉树创建字符串.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode.cn id=606 lang=python3
+#
+# [606] 根据二叉树创建字符串
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return ""
+        if root.left is None and root.right is None:
+            return str(root.val)
+        if root.right is None:
+            return f"{root.val}({self.tree2str(root.left)})"
+        return f"{root.val}({self.tree2str(root.left)})({self.tree2str(root.right)})"
+
+
+# @lc code=end
