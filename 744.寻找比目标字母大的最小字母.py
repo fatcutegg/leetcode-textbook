d3d72ff202bb37875316d61cd072eab37c4b3d0f
!am9a09e <gg9a09e@gmail.com>
2022-10-10 23:18:15 +0800

    744.寻找比目标字母大的最小字母

---
+++ 744.寻找比目标字母大的最小字母.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=744 lang=python3
+#
+# [744] 寻找比目标字母大的最小字母
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        return letters[bisect_right(letters, target)] if target < letters[-1] else letters[0]# @lc code=end
+# @lc code=end
+
