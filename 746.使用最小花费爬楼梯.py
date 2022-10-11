9ef4d2298f5c12247ebad698585a2ea357fc0ef3
!am9a09e <gg9a09e@gmail.com>
2022-10-11 23:19:25 +0800

    746.使用最小花费爬楼梯

---
+++ 746.使用最小花费爬楼梯.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=746 lang=python3
+#
+# [746] 使用最小花费爬楼梯
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[n]
+
+# @lc code=end
+
