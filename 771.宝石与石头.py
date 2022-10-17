fd83d584a0ae0e9be135424d8fef3abf0c09a3c1
!am9a09e <gg9a09e@gmail.com>
2022-10-17 23:24:27 +0800

    771.宝石与石头

---
+++ 771.宝石与石头.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=771 lang=python3
+#
+# [771] 宝石与石头
+#
+
+# @lc code=start
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return sum(s in jewels for s in stones)
+# @lc code=end
+
