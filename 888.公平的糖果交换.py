a5569d62d2105c6fce025581a0dd46d5fe17103f
!am9a09e <gg9a09e@gmail.com>
2022-11-21 22:43:09 +0800

    888.公平的糖果交换

---
+++ 888.公平的糖果交换.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=888 lang=python3
+#
+# [888] 公平的糖果交换
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        sumA, sumB = sum(aliceSizes), sum(bobSizes)
+        delta = (sumA - sumB) // 2
+        rec = set(aliceSizes)
+        ans = None
+        for y in bobSizes:
+            x = y + delta
+            if x in rec:
+                ans = [x, y]
+                break
+        return ans
+
+# @lc code=end
+
