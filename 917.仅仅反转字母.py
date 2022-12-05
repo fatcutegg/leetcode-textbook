d3be645e6706fbff66c1d8ca9610adab40f7edd2
!am9a09e <gg9a09e@gmail.com>
2022-12-05 23:01:59 +0800

    917.仅仅反转字母

---
+++ 917.仅仅反转字母.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=917 lang=python3
+#
+# [917] 仅仅反转字母
+#
+
+# @lc code=start
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        ans = list(s)
+        left, right = 0, len(ans) - 1
+        while True:
+            while left < right and not ans[left].isalpha():  # 判断左边是否扫描到字母
+                left += 1
+            while right > left and not ans[right].isalpha():  # 判断右边是否扫描到字母
+                right -= 1
+            if left >= right:
+                break
+            ans[left], ans[right] = ans[right], ans[left]
+            left += 1
+            right -= 1
+        return ''.join(ans)
+
+# @lc code=end
+
