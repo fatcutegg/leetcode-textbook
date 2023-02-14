d6964590d489080325649af31fcfa6b55cc0de96
!am9a09e <gg9a09e@gmail.com>
2023-02-14 15:21:11 +0800

    2549.统计桌面上的不同数字

---
+++ 2549.统计桌面上的不同数字.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=2549 lang=python3
+#
+# [2549] 统计桌面上的不同数字
+#
+
+
+# @lc code=start
+class Solution:
+
+    def distinctIntegers(self, n: int) -> int:
+        return 1 if n == 1 else n - 1
+
+
+# @lc code=end
