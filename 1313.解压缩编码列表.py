ecd2fb559572289646cb32d6c56f4ff1af0e9f5d
gaoge <gg9a09e@gmail.com>
2023-03-28 17:32:58 +0800

    1313.解压缩编码列表

---
+++ 1313.解压缩编码列表.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=1313 lang=python3
+#
+# [1313] 解压缩编码列表
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(0, len(nums), 2):
+            ans.extend([nums[i + 1]] * nums[i])
+        return ans
+
+
+# @lc code=end
