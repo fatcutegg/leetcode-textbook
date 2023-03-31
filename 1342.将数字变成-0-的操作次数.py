92c5721e8d43dcbdc0404ce1cb4d2fffc4e5e8ae
gaoge <gg9a09e@gmail.com>
2023-03-31 18:08:26 +0800

    1342.将数字变成-0-的操作次数

---
+++ 1342.将数字变成-0-的操作次数.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=1342 lang=python3
+#
+# [1342] 将数字变成 0 的操作次数
+#
+
+
+# @lc code=start
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        ans = 0
+        while num:
+            ans += 1
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+        return ans
+
+
+# @lc code=end
