b5bf664ef53d553c87c1f41c0dbe953832adfc48
gaoge <gg9a09e@gmail.com>
2023-04-07 17:46:22 +0800

    1370.上升下降字符串

---
+++ 1370.上升下降字符串.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=1370 lang=python3
+#
+# [1370] 上升下降字符串
+#
+
+
+# @lc code=start
+class Solution:
+    def sortString(self, s: str) -> str:
+        num = [0] * 26
+        for ch in s:
+            num[ord(ch) - ord('a')] += 1
+
+        ret = list()
+        while len(ret) < len(s):
+            for i in range(26):
+                if num[i]:
+                    ret.append(chr(i + ord('a')))
+                    num[i] -= 1
+            for i in range(25, -1, -1):
+                if num[i]:
+                    ret.append(chr(i + ord('a')))
+                    num[i] -= 1
+
+        return "".join(ret)
+
+
+# @lc code=end
