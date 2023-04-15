cf560f00cbe4ae422c9cbb766a56fd3a9474a9db
!am9a09e <gg9a09e@gmail.com>
2023-04-15 17:46:07 +0800

    577.员工奖金.sql

---
+++ 577.员工奖金.sql
@@ -0,0 +1,11 @@
+--
+-- @lc app=leetcode.cn id=577 lang=mysql
+--
+-- [577] 员工奖金
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select name, bonus from Employee left join Bonus on Employee.empId = Bonus.empId where bonus is null or bonus < 1000
+-- @lc code=end
+
