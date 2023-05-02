24adf8ff5d13353b0047200a159b0a65828cb3e4
!am9a09e <gg9a09e@gmail.com>
2023-05-02 23:44:36 +0800

    1464.数组中两元素的最大乘积

---
+++ 1464.数组中两元素的最大乘积.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1464 lang=python3
+#
+# [1464] 数组中两元素的最大乘积
+#
+
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] - 1) * (nums[-2] - 1)
+
+
+# @lc code=end
