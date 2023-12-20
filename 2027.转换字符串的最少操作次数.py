b07c4f678b947012beaf3f110f795c341a91f169
!am9a09e <gg9a09e@gmail.com>
2023-12-20 20:13:46 +0800

    2027.转换字符串的最少操作次数

---
+++ 2027.转换字符串的最少操作次数.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=2027 lang=python3
+#
+# [2027] 转换字符串的最少操作次数
+#
+
+
+# @lc code=start
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        covered = -1
+        res = 0
+        for i, c in enumerate(s):
+            if c == "X" and i > covered:
+                res += 1
+                covered = i + 2
+        return res
+
+
+# @lc code=end
