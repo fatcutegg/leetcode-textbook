cf650e1fe256affed81717482af919850e2cbd08
!am9a09e <gg9a09e@gmail.com>
2024-01-17 19:51:36 +0800

    2062.统计字符串中的元音子字符串

---
+++ 2062.统计字符串中的元音子字符串.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=2062 lang=python3
+#
+# [2062] 统计字符串中的元音子字符串
+#
+
+
+# @lc code=start
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        n = len(word)
+        res = 0
+        vowelset = set("aeiou")  # 所有元音对应的哈希集合
+        for i in range(n):
+            # 枚举左端点
+            charset = set()  # 子串对应的哈希集合
+            for j in range(i, n):
+                # 按顺序枚举右端点并更新子串哈希集合及比较
+                charset.add(word[j])
+                if charset == vowelset:
+                    res += 1
+        return res
+
+
+# @lc code=end
