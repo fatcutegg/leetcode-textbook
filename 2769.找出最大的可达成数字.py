6aa630c6aa2d1fe3011455a5c46fdb7255d02b25
!am9a09e <gg9a09e@gmail.com>
2024-02-21 00:00:04 +0800

    2769.找出最大的可达成数字

---
+++ 2769.找出最大的可达成数字.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=2769 lang=python3
+#
+# [2769] 找出最大的可达成数字
+#
+
+
+# @lc code=start
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + t * 2
+
+
+# @lc code=end
